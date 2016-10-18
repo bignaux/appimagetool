e40a1e2a51869eb7d4894238b10564bbe23bb56b
Bignaux Ronan <ronan@aimao.org>
2016-10-18 22:53:24 +0200

    fix typo

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ if [ -e /usr/bin/yum ] ; then
   $SUDO yum -y install epel-release
   $SUDO yum -y install git make binutils fuse glibc-devel glib2-devel fuse-devel gcc zlib-devel  libtool xz-devel lz4-devel patch openssl-devel vim-common inotify-tools-devel wget
 
-  # autoconf automake are too older (~2009)
+  # autoconf and automake are too older (~2009)
   wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
   tar xvfvz autoconf-2.69.tar.gz
   cd autoconf-2.69
